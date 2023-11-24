6d867f2590c59a661a1b107d3b041318c4e56816
Hassaan Ahmed <hassaanahmedc@gmail.com>
2023-11-24 20:30:13 +0500

    create rough layout for sliders

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -57,7 +57,7 @@
             </div>
 
             @include('layouts.catalog')
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full mt-6 px-6 py-4 overflow-hidden">
                 {{ $slot }}
             </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,71 @@
 <x-guest-layout>
+    <div class="bg_img_crousel">
+        <div id="sliders_wrapper"
+            class="flex flex-wrap"
+            x-data="{ current_image: 0 }"
+            x-init="setInterval(() => { current_image = (current_image + 1) % 4 }, 2000)">
+            <div id="sliders_container"
+                class="relative justify-center basis-1/2 h-full">
 
+                <img x-cloak
+                    x-transition.duration.1000ms
+                    x-show="current_image === 0"
+                    class="absolute object-cover h-full"
+                    src="{{ Vite::asset('resources/images/slider-1.jpg') }}"
+                    alt="">
 
+                <img x-cloak
+                    x-transition.duration.1000ms
+                    x-show="current_image === 1"
+                    class="absolute object-cover h-full"
+                    src="{{ Vite::asset('resources/images/slider-2.jpg') }}"
+                    alt="">
+
+                <img x-cloak
+                    x-transition.duration.1000ms
+                    x-show="current_image === 2"
+                    class="absolute object-cover h-full"
+                    src="{{ Vite::asset('resources/images/slider-3.jpg') }}"
+                    alt="">
+
+                <img x-cloak
+                    x-transition.duration.1000ms
+                    x-show="current_image === 3"
+                    class="absolute object-cover h-full"
+                    src="{{ Vite::asset('resources/images/slider-4.jpg') }}"
+                    alt="">
+
+                <img src="{{ Vite::asset('resources/images/slider-5.jpg') }}"
+                    alt="">
+            </div>
+            <div class="basis-1/2">
+                <div class="flex flex-wrap">
+                    <figure class="basis-1/2">
+                        <img src="{{ Vite::asset('resources/images/turkish__delight.jpg') }}"
+                            class="w-full h-full aspect-square"
+                            alt="">
+                    </figure>
+
+                    <figure class="basis-1/2">
+                        <img src="{{ Vite::asset('resources/images/chickpeas.jpg') }}"
+                            class="w-full h-full aspect-square"
+                            alt="">
+                    </figure>
+
+                    <figure class="basis-1/2">
+                        <img src="{{ Vite::asset('resources/images/teapot.jpg') }}"
+                            class="w-full h-full aspect-square"
+                            alt="">
+                    </figure>
+
+                    <figure class="basis-1/2">
+                        <img src="{{ Vite::asset('resources/images/free_shipping.jpg') }}"
+                            class="w-full h-full aspect-square"
+                            alt="">
+                    </figure>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </x-guest-layout>
